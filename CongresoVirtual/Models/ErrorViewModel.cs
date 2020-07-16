a2931a8e0de59b6a04981c6254dfc4db27e5b788
areaingenieriacontent <areaingenieria@contentgrp.com>
2020-07-16 13:10:06 -0500

    inicio 0

---
+++ CongresoVirtual/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CongresoVirtual.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
